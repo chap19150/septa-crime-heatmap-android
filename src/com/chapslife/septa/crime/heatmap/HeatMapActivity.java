0cb23cade9a79bd84af5340b20ef2e9ef37f7a1a
kenny <lkchapman@gmail.com>
2014-02-22 09:24:17 -0500

    create project stub

---
+++ src/com/chapslife/septa/crime/heatmap/HeatMapActivity.java
@@ -0,0 +1,15 @@
+package com.chapslife.septa.crime.heatmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HeatMapActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
